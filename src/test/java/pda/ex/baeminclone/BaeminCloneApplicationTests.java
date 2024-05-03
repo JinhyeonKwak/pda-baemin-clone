8adea86afe8d49264866d54283fff80f04e4ca2f
jinhyeon <vmflwu123@gmail.com>
2024-05-03 15:45:36 +0900

    first commit

---
+++ src/test/java/pda/ex/baeminclone/BaeminCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package pda.ex.baeminclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeminCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
